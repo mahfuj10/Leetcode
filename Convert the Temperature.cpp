afc3ae98e5a100f39679c2996f888528c66fac2d
mahfuj10 <mahfujglobal@gmail.com>
2023-05-07 16:31:13 +0600

    convert temp

---
+++ Convert the Temperature.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    vector<double> convertTemperature(double celsius)
+    {
+        double kelvin;
+        double fahrenheit;
+
+        kelvin = celsius + 273.15;
+        fahrenheit = celsius * 1.80 + 32.00;
+
+        return {
+            kelvin,
+            fahrenheit};
+    }
+};
