fc2dacea3237badad4c670f550a1e99355c2a266
mahfuj10 <mahfujglobal@gmail.com>
2023-05-07 16:28:53 +0600

    jwels and stones sollution

---
+++ Jewels and Stones.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int numJewelsInStones(string jewels, string stones)
+    {
+        int count = 0;
+        for (char s : stones)
+        {
+            for (char j : jewels)
+            {
+                if (s == j)
+                {
+                    count++;
+                };
+            };
+        };
+        return count;
+    }
+};
