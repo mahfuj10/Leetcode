969ba5f08a6a7077e9b4a217dfef00d24a6c84f8
mahfuj10 <mahfujglobal@gmail.com>
2023-05-19 16:57:16 +0600

    1561. Maximum Number of Coins You Can Get

---
+++ 1561. Maximum Number of Coins You Can Get.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int maxCoins(vector<int> &piles)
+    {
+        int result = 0, size = piles.size();
+
+        sort(piles.begin(), piles.end());
+
+        for (int i = size / 3; i < size; i += 2)
+        {
+            result += piles[i];
+        };
+
+        return result;
+    }
+};
