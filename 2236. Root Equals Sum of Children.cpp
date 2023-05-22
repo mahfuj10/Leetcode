629371dbbb3206ca23c433830c63ac34ca0413ee
mahfuj10 <mahfujglobal@gmail.com>
2023-05-22 11:03:36 +0600

    2236. Root Equals Sum of Children

---
+++ 2236. Root Equals Sum of Children.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool checkTree(TreeNode *root)
+    {
+        TreeNode *temp = root;
+        return temp->val == temp->left->val + temp->right->val;
+    }
+};
