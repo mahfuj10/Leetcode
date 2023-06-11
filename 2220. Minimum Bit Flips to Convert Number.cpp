6f799b1b084ca3753f70b73033c8e5f467292ae7
mahfuj10 <mahfujglobal@gmail.com>
2023-06-11 18:29:32 +0600

    2220. Minimum Bit Flips to Convert Number

---
+++ 2220. Minimum Bit Flips to Convert Number.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int minBitFlips(int start, int goal)
+    {
+        int res = 0;
+        while (start > 0 || goal > 0)
+        {
+            int temp1 = start % 2;
+            int temp2 = goal % 2;
+            if (temp1 != temp2)
+                res++;
+            start /= 2;
+            goal /= 2;
+        }
+        return res;
+    }
+};
