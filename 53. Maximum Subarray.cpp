d1b2ddce3b057e49e11e4bf2cd910352bfd0ed35
mahfuj10 <mahfujglobal@gmail.com>
2023-07-02 19:28:29 +0600

    maximum subarrays

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -3,16 +3,10 @@ class Solution
 public:
     int maxSubArray(vector<int> &nums)
     {
-        int curr = 0, i, j, n = size(nums), ans = 0;
-        for (i = 0; i < n; ++i)
-        {
-            curr = 0;
-            for (j = i; j < n; ++j)
-            {
-                curr += nums[j];
-                ans = max(ans, curr);
-            }
-        }
-        return ans;
+        int curMax = 0, maxTillNow = INT_MIN;
+        for (auto c : nums)
+            curMax = max(c, curMax + c),
+            maxTillNow = max(maxTillNow, curMax);
+        return maxTillNow;
     }
 };
