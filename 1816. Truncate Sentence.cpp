e1cec07082ccab652ff847df7d567cb0f049d056
mahfuj10 <mahfujglobal@gmail.com>
2023-08-11 13:06:30 +0600

    1816. Truncate Sentence

---
+++ 1816. Truncate Sentence.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    string truncateSentence(string s, int k)
+    {
+        string ans = "";
+        int count = 0;
+
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i] == ' ')
+                count++;
+            if (count == k)
+                break;
+            ans += s[i];
+        }
+
+        return ans;
+    }
+};
