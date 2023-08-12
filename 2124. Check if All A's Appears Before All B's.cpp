1335340449ec6a2b4641b7cf62625a7ccc7553be
mahfuj10 <mahfujglobal@gmail.com>
2023-08-12 22:04:44 +0600

    2124. Check if All A's Appears Before All B's

--- 2124. Check if All A's Appears Before All B's.cpp
+++ 2124. Check if All A's Appears Before All B's.cpp
@@ -5,7 +5,7 @@ public:
     {
         for (int i = 0; i < s.length() - 1; ++i)
         {
-            if (s.charAt(i) == 'b' && s.charAt(i + 1) == 'a')
+            if (s[i] == 'b' && s[i + 1] == 'a')
                 return false;
         }
         return true;
