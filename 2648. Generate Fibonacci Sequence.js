e829ebe9e98984270fe796fc95a11d5b1d4027ab
mahfuj10 <mahfujglobal@gmail.com>
2024-02-18 22:02:46 +0600

    2648. Generate Fibonacci Sequence

---
+++ 2648. Generate Fibonacci Sequence.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let n1 = 0, n2 = 1;
+    while(true){
+        yield n1;
+        let n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
